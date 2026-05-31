3e1482b736d46188b4530589ea1e51781f648a3a
Muhammad Munim Siddiqui <munim1siddiqui@gmail.com>
2026-05-31 13:09:10 +0500

    changes added

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
@@ -15,4 +16,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/mvp-dashboard/',
 }));
